c71472f1401aef488da92a5cec8f8fd7f14540cd
luigi <luigi@macbook-air.home>
2021-11-09 18:57:38 +0100

    third commit

--- prova2021/src/main/java/it/unisa/prova2021/NewMain.java
+++ prova2021/src/main/java/it/unisa/prova2021/NewMain.java
@@ -15,7 +15,7 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello Fisciano");
         System.out.println("Hello World");
         System.out.println("Hello Universe");
         
