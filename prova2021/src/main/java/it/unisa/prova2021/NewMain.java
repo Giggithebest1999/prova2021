88d15906860d642d068b9e9f60b53c43606b9a23
luigi <luigi@macbook-air.home>
2021-11-09 18:52:41 +0100

    second commit

--- prova2021/src/main/java/it/unisa/prova2021/NewMain.java
+++ prova2021/src/main/java/it/unisa/prova2021/NewMain.java
@@ -17,6 +17,7 @@ public class NewMain {
     public static void main(String[] args) {
         
         System.out.println("Hello World");
+        System.out.println("Hello Universe");
         
     }
     
